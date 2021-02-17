721a687df4127b5359a4bb3dc11b09ec6b899966
Rin Raeuber <mail@rin-raeuber.com>
2021-02-17 12:59:31 +0100

    Feature: Display selected files.

--- frontend/src/EmailListUploader.js
+++ frontend/src/EmailListUploader.js
@@ -4,7 +4,13 @@ const EmailListUploader = () => {
   const [uploadedFiles, setUploadedFiles] = useState([]);
 
   const handleUpload = (event) => {
-    if (event.target.files) setUploadedFiles(event.target.files);
+    if (!event.target.files) return;
+
+    // event.target.files is a FileList -> make it an array
+    const fileList = event.target.files;
+    let files = [];
+    for (let i = 0; i < fileList.length; i++) files.push(fileList[i]);
+    setUploadedFiles(files);
   };
 
   const handleSubmit = () => {
@@ -14,7 +20,7 @@ const EmailListUploader = () => {
   return (
     <div>
       <form>
-        <label for="files">
+        <label htmlFor="files">
           Select .txt files containing your email addresses.
         </label>
         <input
@@ -25,8 +31,18 @@ const EmailListUploader = () => {
           onChange={handleUpload}
           multiple
         />
+        {uploadedFiles.length > 0 && (
+          <>
+            <h3>Selected Files</h3>
+            <ul>
+              {uploadedFiles.map((file) => (
+                <li key={`${file.name}-${file.lastModified}`}>{file.name}</li>
+              ))}
+            </ul>
+          </>
+        )}
         <button onClick={handleSubmit} disabled={!uploadedFiles.length}>
-          Upload &amp; Send
+          Send Emails
         </button>
       </form>
     </div>
