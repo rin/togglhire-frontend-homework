4950f92ad918c57bde387b8973422574cb599fbb
Rin Raeuber <mail@rin-raeuber.com>
2021-02-17 17:02:50 +0100

    Feature: Parse and display emails.

--- frontend/src/EmailListUploader.js
+++ frontend/src/EmailListUploader.js
@@ -1,25 +1,57 @@
 import React, { useState } from 'react';
 
+const EMAIL_LIST_DELIMITER = '\n';
+
 const EmailListUploader = () => {
   const [uploadedFiles, setUploadedFiles] = useState([]);
+  const [emails, setEmails] = useState([]);
+  const [hasError, setError] = useState(null);
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
-  const handleUpload = (event) => {
+  const parseEmailsFromFile = async (file) => {
+    return new Promise((resolve, reject) => {
+      let fileReader = new FileReader();
+      fileReader.onload = () => {
+        if (!file.type.match('text')) reject('Please provide a text file.');
+        const content = fileReader.result;
+        const lines = content.trim().split(EMAIL_LIST_DELIMITER);
+        resolve(lines);
+      };
+      fileReader.onerror = reject;
+      fileReader.readAsText(file);
+    });
+  };
+
+  const handleUpload = async (event) => {
     if (!event.target.files) return;
 
     // event.target.files is a FileList -> make it an array
-    const fileList = event.target.files;
-    let files = [];
-    for (let i = 0; i < fileList.length; i++) files.push(fileList[i]);
-    setUploadedFiles(files);
+    const filesArray = Array.from(event.target.files);
+    setUploadedFiles(filesArray);
+
+    Promise.all(filesArray.map(parseEmailsFromFile))
+      .then((result) => {
+        const emailList = result.flat();
+        // using Set to deduplicate emails
+        setEmails([...new Set(emailList)]);
+      })
+      .catch((e) => {
+        console.log(`Something went wrong: ${e}`);
+        setError(e);
+      });
   };
 
   const handleSubmit = () => {
-    // TODO
+    setIsSubmitting(true);
+
+    // on success
+    setIsSubmitting(false);
   };
 
   return (
     <div>
       <form>
+        {hasError && <p>{hasError.message}</p>}
         <label htmlFor="files">
           Select .txt files containing your email addresses.
         </label>
@@ -41,7 +73,20 @@ const EmailListUploader = () => {
             </ul>
           </>
         )}
-        <button onClick={handleSubmit} disabled={!uploadedFiles.length}>
+        {emails.length > 0 && (
+          <>
+            <h3>Emails</h3>
+            <ul>
+              {emails.map((email) => (
+                <li key={email}>{email}</li>
+              ))}
+            </ul>
+          </>
+        )}
+        <button
+          onClick={handleSubmit}
+          disabled={!uploadedFiles.length || isSubmitting}
+        >
           Send Emails
         </button>
       </form>
